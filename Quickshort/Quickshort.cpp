6093003421b4978550b55968c1fb3e383105acaa
anjanipodomi <anjanidihapsari@gmail.com>
2024-03-28 10:25:27 +0700

    menambahkan cout/tampilan "sorted array"

--- Quickshort/Quickshort.cpp
+++ Quickshort/Quickshort.cpp
@@ -85,6 +85,11 @@ void q_short(int low, int high)
     q_short(j + 1, high);
 }
 void display() {
+    cout << "\n---------------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "-----------------" << endl;
+
+
 
  }
 int main()
