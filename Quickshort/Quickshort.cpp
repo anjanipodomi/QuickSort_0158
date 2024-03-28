b8f0b5135bdeb6c9b4febcdbf184a532dd59ed18
anjanipodomi <anjanidihapsari@gmail.com>
2024-03-28 10:18:35 +0700

    menambahkan perulangan while ((arr[j] > pivot) && (j >= low)

--- Quickshort/Quickshort.cpp
+++ Quickshort/Quickshort.cpp
@@ -61,6 +61,13 @@ void q_short(int low, int high)
             i++;
             cmp_count++;
         }
+        cmp_count++;
+
+        while ((arr[j] > pivot) && (j >= low))
+        {
+            j--;
+            cmp_count++;
+        }
     }
 }
 
